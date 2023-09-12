83c0c7d50b5392440384aef421e84e11543aa43b
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-09-11 18:46:51 -0700

    fixed debugging code to run loop and generate in #password

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+//variables 
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var numbers = [1,2,3,4,5,6,7,8,9,0]
 var special = ["!","@","#","$","%","^","&","*","()","+","<",">","?","~","<","/",">"]
+var characters = []
 
-var passwordOptions = []
 
-function randomPass() {
- var pass = passwordOptions[Math.floor(Math.random() * passwordOptions.length)]
-}
 
+//input password length if not a number ,  <8 or >128 will return to start of function 
 function generatePassword () {
-var length = prompt("How many characters do you want your password to be?")
-if ( length < 8 || length > 128 || isNaN(length) ) {
+var passLength = prompt("How many characters do you want your password to be?")
+if ( passLength < 8 || passLength > 128 || isNaN(passLength) ) {
   alert("must be a number betwween 8 and 128 characters!")
 generatePassword()
 } 
@@ -26,7 +24,9 @@ generatePassword()
  var hasNumbers = confirm("would you like to include number characters")
 
  var hasSpecialCharacters = confirm("would you like to include special characters")
- 
+ //variable to put the selected items in the password
+var passwordOptions = []
+
  if (hasLowerCase) {
   passwordOptions = passwordOptions.concat(lowerCase)
  
@@ -44,16 +44,23 @@ generatePassword()
  }
 
 
-
-  for ( var i=0; i < passwordOptions.length; ) {
-  randomPass(passwordOptions)
+//for loop to generate random lowercase, uppercase, numbers and special characters to the length of the input
+  for ( var i=0; i < passLength; i++ ) {
+  var randomPass = Math.floor(Math.random() * passwordOptions.length);
+  var randomChars = passwordOptions[randomPass]
+  console.log(randomChars)
+  characters.push(randomChars)
   }
+  return characters.join("");
 }
+  
+  
+
 
 
  
 
-  
+  //given code 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
